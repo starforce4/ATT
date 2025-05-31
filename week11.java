2a5f49fd24cc719d33dc682d351f83bdf3278e14
starforce4 <dasuswapnika48@gmail.com>
2025-05-31 10:52:19 +0530

    Update week11.java

--- week11.java
+++ week11.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-public class week11{
+public class week9{
 	public static void main(String[] args) throws InterruptedException{
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -20,27 +20,13 @@ public class week11{
 		        WebElement MobileNum=driver.findElement(By.xpath("//*[@id=\"reactPageContent\"]/div/div/div[2]/div[2]/div[1]/input"));
 		        MobileNum.sendKeys("9686548597");
 		        Thread.sleep(1000);
-		        		        
+		        WebElement tn=driver.findElement(By.xpath("//*[@id=\"reactPageContent\"]/div/div/div[2]/div[2]/div[2]/input"));
+		        tn.click();        
 		        WebElement registerbtn=driver.findElement(By.xpath("//*[@id=\"reactPageContent\"]/div/div/div[2]/div[2]/div[3]"));
 		        registerbtn.click();
 		        Thread.sleep(30000);
 		        registerbtn.click();
-		        
-		        WebElement otp1=driver.findElement(By.xpath("//*[@id=\"reactPageContent\"]/div/div[2]/div[1]/input[1]"));
-		        otp1.sendKeys("5"); 	
-		        Thread.sleep(100);
-		        
-		        WebElement otp2=driver.findElement(By.xpath("//*[@id=\"reactPageContent\"]/div/div[2]/div[1]/input[2]"));
-		        otp2.sendKeys("5"); 	
-		        Thread.sleep(100);
-		        
-		        WebElement otp3=driver.findElement(By.xpath("//*[@id=\"reactPageContent\"]/div/div[2]/div[1]/input[3]"));
-		        otp3.sendKeys("4"); 	
-		        Thread.sleep(100);	        
-		        WebElement otp4=driver.findElement(By.xpath("//*[@id=\"reactPageContent\"]/div/div[2]/div[1]/input[4]"));
-		        otp4.sendKeys("8"); 	
-		        Thread.sleep(100);
-		        
+		       
 		        
 		        
 		        }
